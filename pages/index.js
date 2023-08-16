4d2ea383874f9db6187fb6814c9c05f9feac0e77
Alan Erse <alan.erse@vizir.com.br>
2023-08-16 18:46:12 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se a minha princesa ver esta mensagem e der uma risadinha, ela ganha um NYB (pra comer comigo)</h1> 
+    return <h1>Se a minha princesa ver esta mensagem e der uma risadinha, ela ganha um NYB (pra comer comigo) 🍔</h1> 
 }
 
 export default Home;
